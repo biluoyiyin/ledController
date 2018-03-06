f4c9c6352ad70151ac563a43df54af3aaf621e99
xuanchen yao <xuanchen.yao@ucdconnect.ie>
2018-03-06 17:00:56 +0000

    Initailize the project.

---
+++ src/ledcontroller/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+It is main mehod
+"""
+from __future__ import division, print_function, absolute_import
+
+import argparse
+import sys
+import logging
+
+__author__ = "xuanchen yao"
+__copyright__ = "xuanchen yao"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
+
---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+    
+__author__ = "xuanchen yao"
+__copyright__ = "xuanchen yao"
+__license__ = "mit"
+
+import sys
+sys.path.append(".")
+from ledController import main
+ 
