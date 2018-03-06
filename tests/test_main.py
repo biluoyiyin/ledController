52dcb407b1c0c1d6edb687c3aaccc30c7093bb00
xuanchen yao <xuanchen.yao@ucdconnect.ie>
2018-03-06 21:00:48 +0000

    First test case for building Array[][]

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,14 @@ __author__ = "xuanchen yao"
 __copyright__ = "xuanchen yao"
 __license__ = "mit"
 
-import sys
-sys.path.append(".")
-from ledController import main
+import pytest
+from ledController.main import ledController
  
+class Test_main(object):
+	"""docstring for Test_main"""
+	def test_ledController(self):
+		array = ledController(999)
+		assert array.lights.shape == (999, 999)
+
+
+		
