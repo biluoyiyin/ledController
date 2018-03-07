bfa5e2ef259b7cc619dbf12f75d8d7e83be2f783
xuanchen yao <xuanchen.yao@ucdconnect.ie>
2018-03-07 01:12:26 +0000

    test for read the existence of file

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,10 +16,13 @@ class Test_main(object):
 		array = ledController(999)
 		assert array.lights.shape == (999, 999)
 
-	def test_fileExistence(self):
+	#def test_fileExistence(self):
+	#	array = ledController(999)
+	#	file = "./data/test_data.txt"
+	#	assert array.parseFile(file)==True
+    
+	def test_readFile(self):
 		array = ledController(9)
 		file = "./data/test_data.txt"
-		assert array.parseFile(file)==True
-    	
-
-		
+		N, instrucions = array.parseFile(file)
+		assert N, array.parseFile(file)==['turn on 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'switch 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'turn on 2,2 through 7,7\n']
